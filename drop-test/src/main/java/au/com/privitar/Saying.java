74f54a77b1242a9d73ac81eedc89b667b68ff456
Andrew Todd <ajt.other@gmail.com>
2014-05-28 08:56:47 +1000

    initial compiling and working set

---
+++ drop-test/src/main/java/au/com/privitar/Saying.java
@@ -0,0 +1,33 @@
+package au.com.privitar;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * Created by dev on 27/05/2014.
+ */
+public class Saying {
+    private long id;
+
+    @Length(max = 3)
+    private String content;
+
+    public Saying() {
+
+    }
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+}
